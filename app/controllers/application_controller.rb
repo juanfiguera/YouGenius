8321abfb7453629e0bc03f5d72a8a95df9b77561
Juan Figuera <juanfiguera@gmail.com>
2014-10-30 01:44:03 -0400

    remove csrf protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception unless %w(development test).include? Rails.env
+  # protect_from_forgery with: :exception unless %w(development test).include? Rails.env
+
+  # for testing in prod, this is dangerous
+  protect_from_forgery with: :exception unless %w(production development test).include? Rails.env
+
   
 end
